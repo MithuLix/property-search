3057cb7985866d02b4aba950bc578b3c1690fd37
mithulix <mohaiminul.devs@gmail.com>
2023-04-14 07:43:05 +0600

    app

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 
 function App({ Component, pageProps }) {
-  NProgress.configure({ showSpinner: true });
+  NProgress.configure({ showSpinner: false });
 
   Router.events.on('routeChangeStart', () => {
     NProgress.start();
